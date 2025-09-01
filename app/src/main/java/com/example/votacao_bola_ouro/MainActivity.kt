f8c53b60fe0f3fade432a9bb98f15a0858ed4b9a
roger <rogerreinheimer2805@gmail.com>
2025-09-01 17:01:33 -0300

    pagina home

--- app/src/main/java/com/example/votacao_bola_ouro/MainActivity.kt
+++ app/src/main/java/com/example/votacao_bola_ouro/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.votacao_bola_ouro
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +19,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-}
+
+        /*
+        val btnVote = findViewById<AppCompatButton>(R.id.btnVote)
+        btnVote.setOnClickListener {
+            val intent = Intent(this, OutraLayoutAqui1::class.java)
+            startActivity(intent)
+        }
+
+        val bnVote = findViewById<LinearLayout>(R.id.bnVote)
+        bnVote.setOnClickListener {
+            val intent = Intent(this, OutroLayoutAqui1::class.java)
+            startActivity(intent)
+        }
+
+        val bnSobre = findViewById<LinearLayout>(R.id.bnSobre)
+        bnSobre.setOnClickListener {
+            val intent = Intent(this, OutroLayoutAqui2::class.java)
+            startActivity(intent)
+        }
+        */
+
+    }// fim oncreate
+}// fim codigo
